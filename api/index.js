27418cc066e80f8543adc742aba61d483a511840
NetoCruz <galaxy.net0710@gmail.com>
2020-01-17 16:07:48 -0600

    Documentando nuestra api con Swagger

--- api/index.js
+++ api/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const config = require('../config.js');
+const bodyParser = require('body-parser');
 const app = express();
 const user = require('./components/user/network')
+const swaggerUi = require('swagger-ui-express');
 
+app.use(bodyParser.json());
+const swaggerDoc = require('./swagger.json');
 //rutas
 
 app.use('/api/user',user)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
    console.log('APi escuchando en el purto', config.api.port) 
